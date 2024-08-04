02fbe6bfc9c588d0b60478eb670f28335016d139
Deekshat Manhotra <160055819+deekshatmanhotra@users.noreply.github.com>
2024-08-04 21:36:53 +0530

    Update ProductController.java

--- WebApp/src/main/java/com/LearnSpring/WebApp/Controller/ProductController.java
+++ WebApp/src/main/java/com/LearnSpring/WebApp/Controller/ProductController.java
@@ -1,42 +1,99 @@
-package com.LearnSpring.WebApp.Controller;
+package com.LearnSpring.e_com.controller;
 
-import com.LearnSpring.WebApp.Model.Product;
-import com.LearnSpring.WebApp.Service.ProductService;
+import com.LearnSpring.e_com.model.Product;
+import com.LearnSpring.e_com.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
-    ProductService service;
+    private ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getProduct(){
-        return service.getProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products/{prodId}")
-    public Product getProductById(@PathVariable int prodId){
-        return service.getProductById(prodId);
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+        return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/products")
-    public void addProduct(@RequestBody Product prod){
-        service.addProduct(prod);
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+
+
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    @PutMapping("/products")
-    public void updateProduct(@RequestBody Product prod){
-        service.updateProduct(prod);
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+
+        Product product =service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+
     }
 
-    @DeleteMapping("/products/{prodId}")
-    public void deleteProduct(@PathVariable int prodId){
-        service.deleteProduct(prodId);
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id,@RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+        if(product1 != null)
+            return  new ResponseEntity<>("Updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
 
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("deleted", HttpStatus.OK);
+        }else
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+
+
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(String keyword){
+    List<Product> products = service.searchProducts(keyword);
+    return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
 }
